cf1dc53781226ca12e7b24504eb07a3806a1b720
Rayan Mudhayiq <s202249320@kfupm.edu.sa>
2026-04-06 09:56:45 +0300

    TODO 4: Enable CORS middleware

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -6,15 +6,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+import morgan from "morgan";
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote API");
+});
 
 // TODO 6.2: Create "/api/quote" route
+import { getRandomQuote } from "./quotes.js";
 
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
